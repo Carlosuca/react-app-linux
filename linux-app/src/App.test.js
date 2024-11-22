5ab632283da37e2977749bd0ad5723714e5303dd
Carlosuca <00202118@uca.edu.sv>
2024-11-21 23:57:40 -0600

    modify: dockerfile and app.test

--- linux-app/src/App.test.js
+++ linux-app/src/App.test.js
@@ -1,8 +1,13 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { act } from 'react'; // En lugar de react-dom/test-utils
 import App from './App';
 
+
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
