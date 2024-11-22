be5a4e90197d9f67420ce204de9c15fef10641e6
Carlosuca <00202118@uca.edu.sv>
2024-11-22 00:21:42 -0600

    modify: app.test

--- linux-app/src/App.test.js
+++ linux-app/src/App.test.js
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import { act } from 'react'; // Importa act desde React
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
+
 });
