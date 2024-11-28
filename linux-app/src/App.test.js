023008e84f5ce16da417047f8d166b46a5850342
Carlosuca <00202118@uca.edu.sv>
2024-11-27 23:23:33 -0600

    add: app testing

--- linux-app/src/App.test.js
+++ linux-app/src/App.test.js
@@ -6,6 +6,19 @@ describe('App Component', () => {
     render(<App />);
     
     const jugadores = screen.getAllByRole('heading', { level: 2 });
-    expect(jugadores.length).toBeGreaterThanOrEqual(3);
+    expect(jugadores.length).toBeGreaterThanOrEqual(4);
   });
+
+  test('all players in the list have a name', () => {
+    render(<App />);
+
+    // Obtener todos los jugadores renderizados
+    const jugadores = screen.getAllByRole('heading', { level: 2 });
+
+    // Verificar que cada jugador tiene un nombre
+    jugadores.forEach((jugador) => {
+      expect(jugador.textContent).not.toBe('');
+    });
+  });
+
 });
