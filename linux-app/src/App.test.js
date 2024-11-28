d90a6ab1ddde02831c4f228668b815c9481d856a
Carlosuca <00202118@uca.edu.sv>
2024-11-27 19:26:59 -0600

    modify: app.js

--- linux-app/src/App.test.js
+++ linux-app/src/App.test.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { act } from 'react'; // Importa act desde React
 import App from './App';
 
-test('renders learn react link', () => {
-  act(() => {
+describe('App Component', () => {
+  test('renders more than one player on the page', () => {
     render(<App />);
+    
+    const jugadores = screen.getAllByRole('heading', { level: 2 });
+    expect(jugadores.length).toBeGreaterThanOrEqual(3);
   });
 });
